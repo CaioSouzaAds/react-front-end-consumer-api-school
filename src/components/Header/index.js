88fc3e0093870c34d3e9e0a2081e430242cd050c
CaioSouzaAds <caio.cesarads@gmail.com>
2023-05-29 15:25:18 -0300

    pages config and routes in the app

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,24 +2,20 @@ import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Nav } from './styled';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 export default function Header() {
-  const botaoClicado = useSelector((state) => state.example.botaoClicado);
-
   return (
     <>
       <Nav>
         <Link to="/">
           <FaHome size={24} />
         </Link>
-        <Link to="/login">
+        <Link to="/register">
           <FaUserAlt size={24} />
         </Link>
-        <Link to="/hdp">
+        <Link to="/login">
           <FaSignInAlt size={24} />
         </Link>
-        {botaoClicado ? 'Clicado' : 'Não clicado'}
       </Nav>
     </>
   );
